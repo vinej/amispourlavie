5d57f60106876e9a712a6cc54c5b0c76794fd0d7
jyv <jyvinet@hotmail.ca>
2018-03-24 16:18:03 -0400

    purecss

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
 <div style="margin:10px">
-  <img src="chien-japonais.jpg" height="200" width="300"/>
+  <img src="/chien-japonais.jpg" height="200" width="300"/>
   <h1>Amis pour la vie</h1>
   <label for="search">Rechercher: </label>
   <input name="search"/>
